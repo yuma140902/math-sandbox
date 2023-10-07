509ea8b4aa51447fc71b13171ea366daa78240eb
yuma14 <yuma140902@gmail.com>
2023-10-07 22:29:27 +0900

    cleanup

--- src/components/InputEditor.tsx
+++ src/components/InputEditor.tsx
@@ -1,8 +1,5 @@
 import { InputState } from '@/input';
-import { Editor } from '@monaco-editor/react';
-import { Spin, Typography } from 'antd';
-import { editor } from 'monaco-editor';
-import { useRef, useState } from 'react';
+import { Typography } from 'antd';
 import { MonacoEditor } from './util/MonacoEditor';
 
 type Props = {
@@ -12,17 +9,6 @@ type Props = {
 };
 
 export function InputEditor({ input, setInput, isDarkMode }: Props) {
-  const [editorHeight, setEditorHeight] = useState<string | number | undefined>(
-    undefined,
-  );
-
-  const updateHeight = (editor: editor.IStandaloneCodeEditor | null) => {
-    if (editor) {
-      const height = Math.max(40, editor.getContentHeight());
-      setEditorHeight(height);
-    }
-  };
-
   if (input.type === 'latex') {
     // TODO: LaTeXはサポートされていない
     // [4 Steps to Add Custom Language Support to Monaco Editor | by ohdarling | Medium](https://ohdarling88.medium.com/4-steps-to-add-custom-language-support-to-monaco-editor-5075eafa156d)
