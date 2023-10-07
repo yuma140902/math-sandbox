dff22c164a9e109becc4b6f9946f1ea366ee6bef
yuma14 <yuma140902@gmail.com>
2023-10-07 18:58:40 +0900

    update

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,9 @@
-import { Col, Input, Row, Select, Typography } from 'antd';
+import { Button, Card, Col, Input, Row, Select, Typography } from 'antd';
 import { AppLayout } from './presenter/AppLayout';
 import { useState } from 'react';
 import { BlockMath } from 'react-katex';
 import 'katex/dist/katex.min.css';
+import katex from 'katex';
 
 const HEADER_HEIGHT = 64;
 
@@ -15,6 +16,14 @@ function App() {
   const [outputType, setOutputType] = useState<OutputType>('html');
   const [outputText, setOutputText] = useState<string>('');
 
+  const handleConvert = () => {
+    const output = katex.renderToString(latexExpr, {
+      displayMode: true,
+      output: 'html',
+    });
+    setOutputText(output);
+  };
+
   return (
     <AppLayout
       appName="Math Sandbox"
@@ -26,72 +35,92 @@ function App() {
     >
       <Row>
         <Col xs={24} sm={24} md={12} style={{ padding: 10 }}>
-          {/* TODO: Monaco Editor */}
-          <Input.TextArea
-            placeholder="LaTeXの数式を入力"
-            autoSize={{ minRows: 2 }}
-            defaultValue={latexExpr}
-            onChange={(e) => setLatexExpr(e.target.value)}
-          />
-          <Typography.Text>入力タイプ: </Typography.Text>
-          <Select
-            style={{ minWidth: 300 }}
-            defaultValue={inputType}
-            onChange={(value) => setInputType(value)}
-            optionLabelProp="title"
-            options={[
-              {
-                value: 'latex',
-                title: 'LaTeX',
-                label: (
-                  <Typography.Paragraph>
-                    LaTeX
-                    <br />
-                    例:{' '}
-                    <Typography.Text code>
-                      {'\\int_0^\\infty x^2 dx'}
-                    </Typography.Text>
-                  </Typography.Paragraph>
-                ),
-              },
-            ]}
-          />
-          <br />
-          <Typography.Text>出力タイプ: </Typography.Text>
-          <Select
-            style={{ minWidth: 300 }}
-            defaultValue={outputType}
-            onChange={(value) => setOutputType(value)}
-            optionLabelProp="title"
-            options={[
-              {
-                value: 'html',
-                title: 'HTML',
-                label: (
-                  <Typography.Paragraph>
-                    HTML
-                    <br />
-                    <Typography.Text type="secondary">HTML形式</Typography.Text>
-                    <br />
-                    例:{' '}
-                    <Typography.Text code>
-                      {'\\int_0^\\infty x^2 dx'}
-                    </Typography.Text>
-                  </Typography.Paragraph>
-                ),
-              },
-            ]}
-          />
+          <Card title="入力" bordered={false}>
+            {/* TODO: Monaco Editor */}
+            <Input.TextArea
+              placeholder="LaTeXの数式を入力"
+              autoSize={{ minRows: 2 }}
+              defaultValue={latexExpr}
+              onChange={(e) => setLatexExpr(e.target.value)}
+            />
+          </Card>
         </Col>
+
         <Col xs={24} sm={24} md={12} style={{ padding: 10 }}>
-          <Typography.Text>プレビュー</Typography.Text>
-          <Typography.Text>
-            <BlockMath math={latexExpr} />
-          </Typography.Text>
+          <Card title="プレビュー" bordered={false}>
+            <Typography.Text>
+              <BlockMath math={latexExpr} />
+            </Typography.Text>
+          </Card>
+        </Col>
+
+        <Col xs={24} sm={24} md={12} style={{ padding: 10 }}>
+          <Card title="変換の設定" bordered={false}>
+            <Typography.Text>入力タイプ: </Typography.Text>
+            <Select
+              style={{ minWidth: 300 }}
+              defaultValue={inputType}
+              onChange={(value) => setInputType(value)}
+              optionLabelProp="title"
+              options={[
+                {
+                  value: 'latex',
+                  title: 'LaTeX',
+                  label: (
+                    <Typography.Paragraph>
+                      LaTeX
+                      <br />
+                      例:{' '}
+                      <Typography.Text code>
+                        {'\\int_0^\\infty x^2 dx'}
+                      </Typography.Text>
+                    </Typography.Paragraph>
+                  ),
+                },
+              ]}
+            />
+            <br />
+            <Typography.Text>出力タイプ: </Typography.Text>
+            <Select
+              style={{ minWidth: 300 }}
+              defaultValue={outputType}
+              onChange={(value) => setOutputType(value)}
+              optionLabelProp="title"
+              options={[
+                {
+                  value: 'html',
+                  title: 'HTML',
+                  label: (
+                    <Typography.Paragraph>
+                      HTML
+                      <br />
+                      <Typography.Text type="secondary">
+                        HTML形式
+                      </Typography.Text>
+                      <br />
+                      例:{' '}
+                      <Typography.Text code>
+                        {'\\int_0^\\infty x^2 dx'}
+                      </Typography.Text>
+                    </Typography.Paragraph>
+                  ),
+                },
+              ]}
+            />
+            <br />
+            <Button type="primary" onClick={handleConvert}>
+              変換
+            </Button>
+          </Card>
+        </Col>
+
+        <Col xs={24} sm={24} md={12} style={{ padding: 10 }}>
           {/* TODO: シンタックスハイライト*/}
-          <Typography.Paragraph>
-            <pre></pre>
-          </Typography.Paragraph>
+          <Card title="出力" bordered={false}>
+            <Typography.Paragraph>
+              <pre>{outputText}</pre>
+            </Typography.Paragraph>
+          </Card>
         </Col>
       </Row>
     </AppLayout>
