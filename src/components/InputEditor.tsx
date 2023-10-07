6a1724bba4274e67aaf58a09e3c6571ef1f3a22b
yuma14 <yuma140902@gmail.com>
2023-10-07 21:02:28 +0900

    use monaco-editor

--- src/components/InputEditor.tsx
+++ src/components/InputEditor.tsx
@@ -1,5 +1,5 @@
 import { InputState } from '@/input';
-import { Input } from 'antd';
+import { Editor } from '@monaco-editor/react';
 
 type Props = {
   input: InputState;
@@ -8,13 +8,14 @@ type Props = {
 
 export function InputEditor({ input, setInput }: Props) {
   if (input.type === 'latex') {
-    // TODO: Monaco Editor
+    // TODO: LaTeXはサポートされていない
+    // [4 Steps to Add Custom Language Support to Monaco Editor | by ohdarling | Medium](https://ohdarling88.medium.com/4-steps-to-add-custom-language-support-to-monaco-editor-5075eafa156d)
     return (
-      <Input.TextArea
-        placeholder="LaTeXの数式を入力"
-        autoSize={{ minRows: 2 }}
+      <Editor
+        height="90vh"
+        defaultLanguage="latex"
         defaultValue={input.text}
-        onChange={(e) => setInput({ ...input, text: e.target.value })}
+        onChange={(s) => setInput({ ...input, text: s })}
       />
     );
   }
